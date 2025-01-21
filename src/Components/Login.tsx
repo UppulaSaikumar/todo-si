5b7d88d2c54707cb694140360a808831597fa49a
Uppula Saikumar <107742169+UppulaSaikumar@users.noreply.github.com>
2025-01-21 14:46:33 +0530

    Update Login.tsx

--- src/Components/Login.tsx
+++ src/Components/Login.tsx
@@ -1,11 +1,9 @@
-// src/components/Login.tsx
-
-import React from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { googleLogin, logout } from "../redux/actions/authActions";
-import { RootState } from "../redux/store";
-import googleIamge from "../assets/google.png";
-import rightImage from "../assets/rightImage.png";
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { googleLogin, logout } from '../redux/actions/authActions';
+import { RootState } from '../redux/store';
+import googleIamge from '../assets/google.png';
+import rightImage from '../assets/rightImage.png';
 
 const Login: React.FC = () => {
   const dispatch = useDispatch();
@@ -20,7 +18,7 @@ const Login: React.FC = () => {
   };
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 min-h-screen" style={{ backgroundColor: "#FFF9F9" }}>
+    <div className="grid grid-cols-1 md:grid-cols-2 min-h-screen" style={{ backgroundColor: '#FFF9F9' }}>
       {/* Left Section */}
       <div className="flex flex-col items-center justify-center">
         <h1 className="text-4xl font-bold text-gray-800 mb-4">Todo SI </h1>
@@ -37,7 +35,10 @@ const Login: React.FC = () => {
             </button>
           </div>
         ) : (
-          <button onClick={handleGoogleSignIn} className="px-6 py-2 text-white bg-black rounded hover:bg-gray-600 flex items-center space-x-2">
+          <button
+            onClick={handleGoogleSignIn}
+            className="px-6 py-2 text-white bg-black rounded hover:bg-gray-600 flex items-center space-x-2"
+          >
             <img src={googleIamge} alt="Google Logo" className="w-6 h-6 object-contain" />
             <span>Continue with Google</span>
           </button>
